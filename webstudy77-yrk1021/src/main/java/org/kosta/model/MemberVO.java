31040ef1e4ef4f8b1705bcf7ef047cfb975ffd3a
HAYOUNG LEE <hyl9410@gmail.com>
2022-10-21 16:58:05 +0900

    하영 getter/setter 추가

--- webstudy77-yrk1021/src/main/java/org/kosta/model/MemberVO.java
+++ webstudy77-yrk1021/src/main/java/org/kosta/model/MemberVO.java
@@ -3,5 +3,19 @@ package org.kosta.model;
 public class MemberVO {
 	private String name;
 	private String id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
 
 }
